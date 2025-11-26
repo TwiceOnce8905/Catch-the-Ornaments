02291ae2aa77e62ba0aca8a49740eab375c3e940
TwiceOnce8905 <TwiceOnce8905@proton.me>
2025-11-25 21:35:06 -0600

    Ornaments Collide w/ Area
    The ornaments can now collide with the area2d and it also "queue_free"s it. The score system is yet to be added.

--- scirpts/cat.gd
+++ scirpts/cat.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 1000
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -1000
 
 @onready var catani = $catani
 
@@ -27,6 +27,9 @@ func _physics_process(delta):
 		catani.flip_h = false
 	elif direction < 0:
 		catani.flip_h = true
+		
+	if Input.is_action_just_pressed("space") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
 
 	move_and_slide()
--- scirpts/greenorn.gd
+++ scirpts/greenorn.gd
@@ -3,5 +3,12 @@ extends Node2D
 # falling speed, I mean it's literally in the name so
 var fall_speed := 100
 
+func _ready():
+	connect("area_entered", Callable(self, "_on_area_entered"))
+
+func _on_area_entered(area):
+	print("player touch (cause you been so out of touch touch touch touch touch) ornament")
+	queue_free()
+
 func _physics_process(delta):
 	position.y += fall_speed * delta
