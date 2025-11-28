01731ca927e383e92c8bc01902bc805d002ead86
TwiceOnce8905 <TwiceOnce8905@proton.me>
2025-11-27 19:37:00 -0600

    Added End Screen Fully
    Now need to add background and music, edit some placeholder art, then I'm done with the whole game.

--- scirpts/end.gd
+++ scirpts/end.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@export var count_speed: float = 1
+@export var count_speed = 0.05
 
 @onready var collected = $collected
 @onready var _1_star = $"1star"
@@ -8,18 +8,46 @@ extends Control
 @onready var _3_star = $"3star"
 
 var current_score = 0
+var score_done: bool = false
 
 func _ready():
 	collected.visible = false
+	$Button.visible = false
 	collected.text = ""
-	if Global.score >= 8:
-		_1_star.play("default")
-	elif Global.score <= 16:
-		_2_star.play("default")
-	elif Global.score >= 24:
-		_3_star.play("default")
+	play_stars_in_order()
+	
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func play_stars_in_order()-> void :
+	var stars_to_play = []
+	if Global.score >= 12:
+		stars_to_play.append(_1_star)
+	if Global.score >= 20:
+		stars_to_play.append(_2_star)
+	if Global.score >= 25:
+		stars_to_play.append(_3_star)
+	for star_anim in stars_to_play:
+		star_anim.play("default")
+		await star_anim.animation_finished
+	start_score_count()
+	
+func start_score_count():
+	current_score = 0
+	collected.visible = true
+	collected.text = "Ornaments Collected: 0"
+	count_up_score()
+	
+	
+func count_up_score():
+	if Global.score == 0:
+			$Button.visible = true
+	if current_score < Global.score:
+		current_score += 1
+		collected.text = "Ornaments Collected: " + str(current_score)
+		await get_tree().create_timer(count_speed).timeout
+		
+		count_up_score()
+		
+		if current_score >= Global.score:
+			score_done = true
+	if score_done:
+		$Button.visible = true
--- scirpts/timeandscore.gd
+++ scirpts/timeandscore.gd
@@ -5,18 +5,15 @@ var timer_started := false
 
 func _ready():
 	$Timer/Label.visible = false
-	$Label.visible = false
-	
 
 func _process(delta):
 	if Global.started and not timer_started:
 		$Timer/Label.visible = true
-		$Label.visible = true
 		timer.start()
 		timer_started = true
 	$Timer/Label.text = "⏱️: " + str(snapped(timer.time_left, 0))
-	$Label.text = "Ornaments Collected: " + str(Global.score)
 
 func _on_timer_timeout():
 	print(Global.score)
-	get_tree().quit()
+	Global.started = false
+	get_tree().change_scene_to_file("res://scenes/end.tscn")
